a76c8d81c96b980d55a4a84b38374ffc6c4aa6e7
Cosmin <cosminflavius23@gmail.com>
2025-03-17 13:34:03 +0200

    i modified the project

---
+++ CalculatorApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CalculatorApp.Views;
+
+namespace CalculatorApp
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private object _currentViewModel;
+        public object CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                _currentViewModel = value;
+                OnPropertyChanged(nameof(CurrentViewModel));
+            }
+        }
+
+        public ICommand SchimbaInterfataCommand { get; }
+
+        public MainViewModel()
+        {
+            // Inițial, afișăm prima interfață
+            CurrentViewModel = new View1();
+            SchimbaInterfataCommand = new RelayCommand(param => SchimbaInterfata());
+        }
+
+        private void SchimbaInterfata()
+        {
+            // Comută între ViewModel1 și ViewModel2
+            if (CurrentViewModel is View1)
+                CurrentViewModel = new View2();
+            else
+                CurrentViewModel = new View1();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+}
