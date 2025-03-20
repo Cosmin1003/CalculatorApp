22505ddee94b8888d37379fa233d0db5118b938d
Cosmin <cosminflavius23@gmail.com>
2025-03-20 19:56:09 +0200

    Programmer functionality

---
+++ CalculatorApp/Models/ProgrammerCalculatorEngine.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp.Models
+{
+    public class ProgrammerCalculatorEngine
+    {
+        private long _previousValue;
+        private char _pendingOperator;
+        private bool _isNewEntry;
+        private string _currentEntry;
+
+        public ProgrammerCalculatorEngine()
+        {
+            ClearAll();
+        }
+
+        // Proprietate pentru afișajul curent (în format hex)
+        public string CurrentEntry => _currentEntry;
+
+        // Permite setarea directă a intrării curente (de exemplu, dintr-o selecție din istoric)
+        public void SetCurrentEntry(string entry)
+        {
+            _currentEntry = entry;
+            _isNewEntry = true;
+        }
+
+        // Adaugă o cifră sau literă (0-9, A-F)
+        public void InputDigit(string digit)
+        {
+            if (_isNewEntry || _currentEntry == "0")
+            {
+                _currentEntry = digit;
+                _isNewEntry = false;
+            }
+            else
+            {
+                _currentEntry += digit;
+            }
+        }
+
+        // Setează operatorul (+, -, *, /, %). Dacă se introduce un nou operator, se efectuează calculul curent.
+        public void SetOperator(char op)
+        {
+            if (!_isNewEntry)
+            {
+                Calculate();
+            }
+            _pendingOperator = op;
+            _previousValue = ParseHex(_currentEntry);
+            _isNewEntry = true;
+        }
+
+        // Efectuează calculul folosind operatorul curent
+        public void Calculate()
+        {
+            long currentNumber = ParseHex(_currentEntry);
+            long result = currentNumber;
+            if (_pendingOperator != '\0')
+            {
+                switch (_pendingOperator)
+                {
+                    case '+':
+                        result = _previousValue + currentNumber;
+                        break;
+                    case '-':
+                        result = _previousValue - currentNumber;
+                        break;
+                    case '*':
+                        result = _previousValue * currentNumber;
+                        break;
+                    case '/':
+                        if (currentNumber == 0)
+                            throw new DivideByZeroException("Division by zero is not allowed.");
+                        result = _previousValue / currentNumber;
+                        break;
+                    case '%':
+                        result = _previousValue % currentNumber;
+                        break;
+                }
+                _currentEntry = FormatHex(result);
+                _pendingOperator = '\0';
+                _previousValue = result;
+                _isNewEntry = true;
+            }
+        }
+
+        // Șterge doar intrarea curentă (CE)
+        public void ClearEntry()
+        {
+            _currentEntry = "0";
+            _isNewEntry = true;
+        }
+
+        // Resetează complet calculatorul (C)
+        public void ClearAll()
+        {
+            _currentEntry = "0";
+            _previousValue = 0;
+            _pendingOperator = '\0';
+            _isNewEntry = true;
+        }
+
+        // Șterge ultimul caracter (Backspace)
+        public void Backspace()
+        {
+            if (!_isNewEntry && _currentEntry.Length > 0)
+            {
+                _currentEntry = _currentEntry.Substring(0, _currentEntry.Length - 1);
+                if (string.IsNullOrEmpty(_currentEntry) || _currentEntry == "-")
+                {
+                    _currentEntry = "0";
+                    _isNewEntry = true;
+                }
+            }
+        }
+
+        // Schimbă semnul (±)
+        public void ChangeSign()
+        {
+            if (_currentEntry.StartsWith("-"))
+                _currentEntry = _currentEntry.Substring(1);
+            else if (_currentEntry != "0")
+                _currentEntry = "-" + _currentEntry;
+        }
+
+        // Metodă ajutătoare pentru a parsa intrarea curentă (format hex) într-un număr întreg
+        private long ParseHex(string entry)
+        {
+            bool isNegative = entry.StartsWith("-");
+            string valueString = isNegative ? entry.Substring(1) : entry;
+            long value = 0;
+            try
+            {
+                // Convertim string-ul în număr, folosind baza 16
+                value = Convert.ToInt64(valueString, 16);
+            }
+            catch
+            {
+                value = 0;
+            }
+            return isNegative ? -value : value;
+        }
+
+        // Formatează un număr întreg în șir hexazecimal (cu litere majuscule)
+        private string FormatHex(long value)
+        {
+            if (value < 0)
+                return "-" + Convert.ToString(-value, 16).ToUpper();
+            else
+                return Convert.ToString(value, 16).ToUpper();
+        }
+
+        // Metode de conversie pentru a obține reprezentarea în diferite baze
+        public string ToHex()
+        {
+            long value = ParseHex(_currentEntry);
+            return FormatHex(value);
+        }
+
+        public string ToDec()
+        {
+            long value = ParseHex(_currentEntry);
+            return value.ToString();
+        }
+
+        public string ToOct()
+        {
+            long value = ParseHex(_currentEntry);
+            return Convert.ToString(value, 8);
+        }
+
+        public string ToBin()
+        {
+            long value = ParseHex(_currentEntry);
+            return Convert.ToString(value, 2);
+        }
+    }
+}
--- CalculatorApp/ViewModels/ProgrammerModeViewModel.cs
+++ CalculatorApp/ViewModels/ProgrammerModeViewModel.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using CalculatorApp.Views;
 using System.ComponentModel;
 using System.Windows.Input;
+using CalculatorApp.Models;
 
 namespace CalculatorApp.ViewModels
 {
@@ -23,16 +24,154 @@ namespace CalculatorApp.ViewModels
             }
         }
 
+
+        private ProgrammerCalculatorEngine _calculator;
+        private string _display;
+        public string Display
+        {
+            get => _display;
+            set
+            {
+                _display = value;
+                OnPropertyChanged(nameof(Display));
+                UpdateConversions();
+            }
+        }
+
+        // Proprietăți pentru afișarea conversiilor
+        private string _hexValue;
+        public string HexValue
+        {
+            get => _hexValue;
+            set { _hexValue = value; OnPropertyChanged(nameof(HexValue)); }
+        }
+
+        private string _decValue;
+        public string DecValue
+        {
+            get => _decValue;
+            set { _decValue = value; OnPropertyChanged(nameof(DecValue)); }
+        }
+
+        private string _octValue;
+        public string OctValue
+        {
+            get => _octValue;
+            set { _octValue = value; OnPropertyChanged(nameof(OctValue)); }
+        }
+
+        private string _binValue;
+        public string BinValue
+        {
+            get => _binValue;
+            set { _binValue = value; OnPropertyChanged(nameof(BinValue)); }
+        }
+
+        // Comenzi pentru operații și cifre
+        public ICommand DigitCommand { get; }
+        public ICommand OperatorCommand { get; }
+        public ICommand EqualCommand { get; }
+        public ICommand ClearCommand { get; }
+        public ICommand ClearEntryCommand { get; }
+        public ICommand BackspaceCommand { get; }
+        public ICommand ChangeSignCommand { get; }
+
+
         public ICommand ShowProgrammerModeButtonsCommand { get; }
         public ICommand ShowProgrammerModeBinaryCommand { get; }
 
         public ProgrammerModeViewModel()
         {
+            _calculator = new ProgrammerCalculatorEngine();
+            Display = _calculator.CurrentEntry;
+
+            DigitCommand = new RelayCommand(param => AppendDigit(param.ToString()));
+            OperatorCommand = new RelayCommand(param => SetOperator(param.ToString()[0]));
+            EqualCommand = new RelayCommand(param => CalculateResult());
+            ClearCommand = new RelayCommand(param => ClearAll());
+            ClearEntryCommand = new RelayCommand(param => ClearEntry());
+            BackspaceCommand = new RelayCommand(param => Backspace());
+            ChangeSignCommand = new RelayCommand(param => ChangeSign());
+
+            UpdateConversions();
+
             // Setăm interfața implicită:
             CurrentInterface = new ProgrammerModeButtons();
 
             ShowProgrammerModeButtonsCommand = new RelayCommand(_ => CurrentInterface = new ProgrammerModeButtons());
             ShowProgrammerModeBinaryCommand = new RelayCommand(_ => CurrentInterface = new ProgrammerModeBinary());
+        }
+
+        private void AppendDigit(string digit)
+        {
+            try
+            {
+                _calculator.InputDigit(digit);
+                Display = _calculator.CurrentEntry;
+            }
+            catch
+            {
+                Display = "Error";
+            }
+        }
+
+        private void SetOperator(char op)
+        {
+            try
+            {
+                _calculator.SetOperator(op);
+                Display = _calculator.CurrentEntry;
+            }
+            catch
+            {
+                Display = "Error";
+            }
+        }
+
+        private void CalculateResult()
+        {
+            try
+            {
+                _calculator.Calculate();
+                Display = _calculator.CurrentEntry;
+            }
+            catch
+            {
+                Display = "Error";
+            }
+        }
+
+        private void ClearAll()
+        {
+            _calculator.ClearAll();
+            Display = _calculator.CurrentEntry;
+        }
+
+        private void ClearEntry()
+        {
+            _calculator.ClearEntry();
+            Display = _calculator.CurrentEntry;
+        }
+
+        private void Backspace()
+        {
+            _calculator.Backspace();
+            Display = _calculator.CurrentEntry;
+        }
+
+        private void ChangeSign()
+        {
+            _calculator.ChangeSign();
+            Display = _calculator.CurrentEntry;
+        }
+
+        // Actualizează proprietățile de conversie pe baza valorii curente
+        private void UpdateConversions()
+        {
+            HexValue = _calculator.ToHex();
+            DecValue = _calculator.ToDec();
+            OctValue = _calculator.ToOct();
+            BinValue = _calculator.ToBin();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
